9265978d2fa1db99f8ef68908967a5ba3a447b29
Gonzalo Sanchez <gonza.sanchez@gmail.com>
2023-10-12 10:20:46 -0300

    ADD client /clients & client/id services

--- clase07/index.js
+++ clase07/index.js
@@ -1,26 +1,45 @@
-var express = require('express')
+var express = require("express");
 
-var products = require('./data/products.json')
+var products = require("./data/products.json");
+var clients = require("./data/clients.json");
 
-var app = express()
-var port = 3000
+var app = express();
+var port = 3000;
 
-app.get('/', function(req, res) {
-  res.send('<h1>Hello '+Date.now()+'!</h1>')
-})
+// general
 
-app.get('/products', function(req, res) {
-    res.json(products.list)
-})
+app.get("/", function (req, res) {
+  res.send("<h1>Hello " + Date.now() + "!</h1>");
+});
 
-app.get('/products/:id', function(req, res) {
-    var product = products.list.find(function(product){
-        return parseInt(product.id) === parseInt(req.params.id)
-    })
-    if(product) res.json(product)
-    else res.sendStatus(404)
-})
+app.listen(port, function () {
+  console.log("Example app listening on port " + port);
+});
 
-app.listen(port, function() {
-  console.log('Example app listening on port '+port)
-})
+// products
+
+app.get("/products", function (req, res) {
+  res.json(products.list);
+});
+
+app.get("/products/:id", function (req, res) {
+  var product = products.list.find(function (product) {
+    return parseInt(product.id) === parseInt(req.params.id);
+  });
+  if (product) res.json(product);
+  else res.sendStatus(404);
+});
+
+// clients
+
+app.get("/clients", function (req, res) {
+  res.json(clients.list);
+});
+
+app.get("/clients/:id", function (req, res) {
+  var client = clients.list.find(function (client) {
+    return parseInt(client.id) === parseInt(req.params.id);
+  });
+  if (client) res.json(client);
+  else res.sendStatus(404);
+});
