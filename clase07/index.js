2aef87f240844d8f2725ab00e710fa8111f2938c
Gonzalo Sanchez <gonza.sanchez@gmail.com>
2023-10-27 13:44:02 -0300

    ADD client post

--- clase07/index.js
+++ clase07/index.js
@@ -1,8 +1,8 @@
-var express = require("express");
-const fs = require("fs");
+var express = require('express');
+const fs = require('fs');
 
-var products = require("./data/products.json");
-var clients = require("./data/clients.json");
+var products = require('./data/products.json');
+var clients = require('./data/clients.json');
 
 var app = express();
 app.use(express.json());
@@ -10,22 +10,22 @@ var port = 3000;
 
 // general
 
-app.get("/", function (req, res) {
-  res.send("<h1>Hello " + Date.now() + "!</h1>");
+app.get('/', function (req, res) {
+  res.send('<h1>Hello ' + Date.now() + '!</h1>');
 });
 
 app.listen(port, function () {
-  console.log("Example app listening on port " + port);
+  console.log('Example app listening on port ' + port);
 });
 
 // write
 
 function writeData(type, data, callback) {
   console.log(data);
-  fs.writeFile(
-    type + ".json",
+  fs.writeFileSync(
+    './data/' + type + '.json',
     JSON.stringify(data, null, 2),
-    "utf8",
+    'utf8',
     (error) => {
       if (error) {
         callback(error);
@@ -38,11 +38,11 @@ function writeData(type, data, callback) {
 
 // products
 
-app.get("/products", function (req, res) {
+app.get('/products', function (req, res) {
   res.json(products.list);
 });
 
-app.get("/products/:id", function (req, res) {
+app.get('/products/:id', function (req, res) {
   var product = products.list.find(function (product) {
     return parseInt(product.id) === parseInt(req.params.id);
   });
@@ -50,7 +50,7 @@ app.get("/products/:id", function (req, res) {
   else res.sendStatus(404);
 });
 
-app.post("/products", (req, res) => {
+app.post('/products', (req, res) => {
   const newProduct = {
     id: req.body.id,
     prod: req.body.prod,
@@ -59,9 +59,9 @@ app.post("/products", (req, res) => {
 
   products.list.push(newProduct);
 
-  writeData("products", newProduct, (writeError) => {
+  writeData('products', newProduct, (writeError) => {
     if (writeError) {
-      res.status(500).send("Internal Server Error");
+      res.status(500).send('Internal Server Error');
       return;
     }
     res.json(newProduct);
@@ -70,14 +70,28 @@ app.post("/products", (req, res) => {
 
 // clients
 
-app.get("/clients", function (req, res) {
+app.get('/clients', function (_, res) {
   res.json(clients.list);
 });
 
-app.get("/clients/:id", function (req, res) {
+app.get('/clients/:id', function (req, res) {
   var client = clients.list.find(function (client) {
     return parseInt(client.id) === parseInt(req.params.id);
   });
   if (client) res.json(client);
   else res.sendStatus(404);
+});
+
+app.post('/clients', (req, res) => {
+  const newClient = req.body;
+  newClient.id = clients.list[clients.list.length - 1].id + 1;
+  clients.list.push(newClient);
+
+  writeData('clients', clients, (writeError) => {
+    if (writeError) {
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+    res.status(201).json(newClient);
+  });
 });
